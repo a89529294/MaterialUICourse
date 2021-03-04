fbd1da4f68e200ec18198bf3cee3c6ced0cd9fc8
Albert Chang <a89529294@hotmail.com>
2021-03-04 21:00:54 +0800

    end of section 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
@@ -7,13 +8,34 @@ import theme from './ui/Theme';
 
 function App() {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
         <Header />
-        hello
-      </ThemeProvider>
-    </React.Fragment>
+        <Switch>
+          <Route exact path="/" component={() => <div>home</div>} />
+          <Route exact path="/services" component={() => <div>services</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>customsoftware</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>mobileapps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>about</div>} />
+          <Route exact path="/contact" component={() => <div>contact</div>} />
+          <Route exact path="/estimate" component={() => <div>estimate</div>} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.svg';
 
@@ -26,7 +28,11 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
   },
   logo: {
-    height: '7rem',
+    height: '8rem',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': { backgroundColor: 'transparent' },
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -36,24 +42,98 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 10,
     marginLeft: 25,
   },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    marginLeft: 50,
+    marginRight: 25,
+    height: 45,
+  },
 }));
 
 export default function Header(props) {
   const classes = useStyles();
+
+  const [value, setValue] = useState(0);
+
+  const onChangeCallback = (e, newValue) => setValue(newValue);
+
+  useEffect(() => {
+    const pathname = window.location.pathname;
+    if (pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  }, []);
 
   return (
     <>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img src={logo} alt="company logo" className={classes.logo} />
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+              disableRipple
+            >
+              <img src={logo} alt="company logo" className={classes.logo} />
+            </Button>
+
+            <Tabs
+              className={classes.tabContainer}
+              value={value}
+              onChange={onChangeCallback}
+              indicatorColor="primary"
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
             </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
